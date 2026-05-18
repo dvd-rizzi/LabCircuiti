827d71b78ed43b7ab25cff5d032521aed95eeb94
Davide <davide.rizzi4@studio.unibo.it>
2026-05-18 13:19:20 +0200

    Altri dati

--- main.cpp
+++ main.cpp
@@ -1,13 +1,13 @@
 #include "F_Voltage.hpp"
 
 int main() {
-    parameters p1{"Dati_modificati/100ohm/sweep4_freq", 100, 470e-6, 0.9e-6, 2.5, 50};
-    parameters p2{"Dati_nuovi/330O/sweep2_330O_freq.txt", 330, 470e-6, 0.9e-6, 2.5, 50};
+    parameters p1{"Dati/100O/sweep1_100O_freq", 100, 486e-6, 0.9e-6, 2.5, 50};
+    parameters p2{"Dati/330O/sweep2_330O_freq", 330, 470e-6, 0.9e-6, 2.5, 50};
     parameters p3{"Dati_modificati/560ohm/sweep1_freq_560O", 560, 470e-6, 0.9e-6, 2.5, 50};
     
-    voltage_function(p2);
-    response_function(p2);
-    v_fit(p2);
+    voltage_function(p1);
+    response_function(p1);
+    v_fit(p1);
 
-    gaussian_error("Dati_modificati/Misure_preliminari/Fixed_voltage_1");
+    //gaussian_error("Dati_modificati/Misure_preliminari/Fixed_voltage_1");
 }
