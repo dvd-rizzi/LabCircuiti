47dcb9db029210963e4a911ebd553337547b92e8
Davide <davide.rizzi4@studio.unibo.it>
2026-06-09 23:10:43 +0200

    animazione

--- cool_graph.C
+++ cool_graph.C
@@ -1,7 +1,7 @@
 #include "F_Voltage.hpp"
 
 void cool_graph() {
-   TCanvas *C = new TCanvas("C", "Sweep 3D", 1000, 800);
+   TCanvas *C = new TCanvas("C", "Sweep 3D", 2000, 1600);
     
     TGraph2D *graph = new TGraph2D();
     TGraph2D *proj_volt = new TGraph2D();
@@ -69,6 +69,74 @@ void cool_graph() {
 
     gPad->SetPhi(35);
     gPad->SetTheta(30);
+
+  int frame_id = 0;
+    
+    double target1_theta = 0.1;   
+    double target1_phi   = -90.0; 
+
+    double target2_theta = 90.0;  
+    double target2_phi   = 0.0;   
+
+    double target3_theta = 0.1;   
+    double target3_phi   = 0.0;   
+
+    int fase1_frames = 180;
+    for (int f = 0; f < fase1_frames; ++f) {
+        double t = (double)f / (fase1_frames - 1);
+        double current_theta = 30.0 + t * (target1_theta - 30.0);
+        double current_phi   = 35.0 + t * (target1_phi - 35.0);
+        
+        gPad->SetTheta(current_theta);
+        gPad->SetPhi(current_phi);
+        C->Modified(); C->Update();
+        C->SaveAs(Form("frames/frame_%04d.png", frame_id++)); // Usiamo %04d perché supereremo i 999 frame totali
+    }
+
+    for (int f = 0; f < 60; ++f) {
+        gPad->SetTheta(target1_theta);
+        gPad->SetPhi(target1_phi);
+        C->Modified(); C->Update();
+        C->SaveAs(Form("frames/frame_%04d.png", frame_id++));
+    }
+
+    int fase2_frames = 180;
+    for (int f = 0; f < fase2_frames; ++f) {
+        double t = (double)f / (fase2_frames - 1);
+        double current_theta = target1_theta + t * (target2_theta - target1_theta);
+        double current_phi   = target1_phi   + t * (target2_phi   - target1_phi);
+        
+        gPad->SetTheta(current_theta);
+        gPad->SetPhi(current_phi);
+        C->Modified(); C->Update();
+        C->SaveAs(Form("frames/frame_%04d.png", frame_id++));
+    }
+
+    for (int f = 0; f < 60; ++f) {
+        gPad->SetTheta(target2_theta);
+        gPad->SetPhi(target2_phi);
+        C->Modified(); C->Update();
+        C->SaveAs(Form("frames/frame_%04d.png", frame_id++));
+    }
+
+    int fase3_frames = 120;
+    for (int f = 0; f < fase3_frames; ++f) {
+        double t = (double)f / (fase3_frames - 1);
+        double current_theta = target2_theta + t * (target3_theta - target2_theta);
+        double current_phi   = target2_phi   + t * (target3_phi   - target2_phi);
+        
+        gPad->SetTheta(current_theta);
+        gPad->SetPhi(current_phi);
+        C->Modified(); C->Update();
+        C->SaveAs(Form("frames/frame_%04d.png", frame_id++));
+    }
+
+    for (int f = 0; f < 60; ++f) {
+        gPad->SetTheta(target3_theta);
+        gPad->SetPhi(target3_phi);
+        C->Modified(); C->Update();
+        C->SaveAs(Form("frames/frame_%04d.png", frame_id++));
+    }
 
     C->SaveAs("3D_graph.pdf");
 }
